c998e39e5f7bec8c84235a32998d2ba7b172854c
akilcena45 <70029742+akilcena45@users.noreply.github.com>
2025-01-13 10:12:42 +0800

    Update user_update.php

--- auth/user_update.php
+++ auth/user_update.php
@@ -41,7 +41,7 @@ $full_name = $user['full_name'];
 
 <body>
     <nav id='navBar' class='navbar-white'>
-        <a href='../index.php' class='logo'> triptrip </a>
+        <a href='../index.php' class='logo'> Kampung Sting </a>
         <ul class='nav-links'>
             <li><a href='../index.php'>Popular Places</a></li>
             <li><a href='../listing.php'>All packages</a></li>
