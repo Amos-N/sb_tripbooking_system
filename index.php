8bbd70b187305a0c52161940ab8ac7beb7ec9b84
akilcena45 <70029742+akilcena45@users.noreply.github.com>
2025-01-13 09:50:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -45,25 +45,10 @@
             <div>
                 <img src="https://4.bp.blogspot.com/-mdhiT5vtA_A/TyZ6nLh1RDI/AAAAAAAAAA0/wPKDcNBcUTM/s1600/sundarban+forest3.jpg">
                 <span>
-                    <h3>Sundarbans</h3>
-                    <p>Starts @ 3000 Taka</p>
+                    <h3>Overnight</h3>
+                    <p>Starts RM150 </p>
                 </span>
             </div>
-            <div>
-                <img src="https://th.bing.com/th/id/R.25148796f3a95244b27a261256084c4b?rik=KmmFNVd97%2bjhog&pid=ImgRaw&r=0">
-                <span>
-                    <h3>Bandarban</h3>
-                    <p>Starts @ 3500 Taka</p>
-                </span>
-            </div>
-            <div>
-                <img src="https://th.bing.com/th/id/OIP.Y7VuBeaYWrUrIl1l8KWwkQHaEK?pid=ImgDet&rs=1">
-                <span>
-                    <h3>Saint Martin</h3>
-                    <p>Starts @ 5000 Taka</p>
-                </span>
-            </div>
-        </div>
 
         <!-- -------------------Trending Places------------- -->
 
@@ -71,19 +56,19 @@
         <div class="tranding">
             <div>
                 <img src="https://th.bing.com/th/id/OIP.HBapS0SdsaLKwebDgCzW9gHaEO?pid=ImgDet&rs=1">
-                <h3>Sajek Valley</h3>
+                <h3>Waterfall</h3>
             </div>
             <div>
                 <img src="https://th.bing.com/th/id/OIP.pmLRmfoPqY9l7tWUasGoAgHaES?pid=ImgDet&rs=1">
-                <h3>Ratargul Swamp Forest</h3>
+                <h3>Hiking Trail</h3>
             </div>
             <div>
                 <img src="https://i.ytimg.com/vi/eFrr7jWJ9q8/maxresdefault.jpg">
-                <h3>Inani Beach</h3>
+                <h3>View from the clouds</h3>
             </div>
             <div>
                 <img src="https://th.bing.com/th/id/OIP.F13moM_Fj9DVoRNKvtK6FwHaEK?pid=ImgDet&rs=1">
-                <h3>Kuakata Sea Beach</h3>
+                <h3>River tour</h3>
             </div>
         </div>
 
@@ -101,7 +86,7 @@
         <div class="stories">
             <div class="travellers-card">
                 <img src="https://th.bing.com/th/id/R.4333832c4bf1ac8a33b7de33edf931f6?rik=L5WmyYEBGCsuLQ&riu=http%3a%2f%2fblogs.ubc.ca%2ftheglobalspectrum%2ffiles%2f2015%2f11%2flets-travel-to-bangladesh-with-simon-urwin-featured.jpg&ehk=xhl0gLFAo9b9whN8uY4zQflUaC7EkHLpJE0XWQUVf3I%3d&risl=&pid=ImgRaw&r=0">
-                <p><a href="https://awaywiththesteiners.com/travel-in-bangladesh/">Travelling in Bangladesh Starter</a>
+                <p><a href="https://awaywiththesteiners.com/travel-in-bangladesh/">Travelling in Sarawak</a>
                 </p>
             </div>
             <div class="travellers-card">
